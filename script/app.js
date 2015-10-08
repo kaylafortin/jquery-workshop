3a93ac39793d44bd183b489cf18464c44ee2f7cd
hennigk <kayla.hennig@gmail.com>
2015-10-08 22:03:24 +0000

    exercise-2

--- script/app.js
+++ script/app.js
@@ -1,9 +1,19 @@
-$("img").mouseover(function(){
-  $(this).css({ width: "110%" });  
-})
-$("img").mouseout(function(){
-  $(this).css({ width: "100%" });  
-})
+
+
 $("img").click(function(){
-  $("body").addClass('overlay')
+  var src = ($(this).attr("src"))
+  var closeButton = $("<button class='xButton'>X</button>");
+  var image = "<img src='" + src + "'>"
+  var overlay = $('<div class="overlay"></div>');
+  var imageBox= $('<div class="image"></div>');
+  var alt = ($(this).attr("alt"));
+  var text = ('<figcaption>' + alt + '</figcaption>');
+  $(closeButton).appendTo(imageBox);
+  $(image).appendTo(imageBox);
+  $(text).appendTo(imageBox);
+  $(imageBox).appendTo(overlay);
+  $(overlay).appendTo(document.body);
+  $(".xButton").click(function(){
+    $(overlay).remove();
+})
 })
